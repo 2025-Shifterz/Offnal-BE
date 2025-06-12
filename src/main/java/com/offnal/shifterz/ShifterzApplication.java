81f8ea16b047b72af743a18ba83b0cd3740e121c
yunnij <blrdyunnij@gmail.com>
2025-06-13 05:18:14 +0900

    [chore][yunnij] - Spring Boot 초기 프로젝트 세팅

---
+++ src/main/java/com/offnal/shifterz/ShifterzApplication.java
@@ -0,0 +1,13 @@
+package com.offnal.shifterz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShifterzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShifterzApplication.class, args);
+	}
+
+}
