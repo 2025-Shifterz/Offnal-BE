078c454b874a365693d110c85e52753ba2d65e18
GOOHAESEUNG <amu6675@naver.com>
2025-06-27 15:07:51 +0900

    [FEAT][GOOHAESEUNG] 로그인 성공 에러 응답 코드 스웨거 예시 추가 #8

--- src/main/java/com/offnal/shifterz/kakao/KakaoLoginController.java
+++ src/main/java/com/offnal/shifterz/kakao/KakaoLoginController.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -49,7 +50,19 @@ public class KakaoLoginController {
                             )
                     )
             ),
-            @ApiResponse(responseCode = "500", description = "서버 내부 오류 발생")
+            @ApiResponse(responseCode = "500", description = "서버 내부 오류 발생",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class),
+                            examples = @ExampleObject(
+                                    name = "에러 예시",
+                                    value = "{\n" +
+                                            "  \"code\": \"INTERNAL_SERVER_ERROR\",\n" +
+                                            "  \"message\": \"서버 내부 오류가 발생했습니다.\"\n" +
+                                            "}"
+                            )
+                    )
+            )
     })
     @GetMapping("/callback")
     public ResponseEntity<?> callback(@RequestParam("code") String code) {
