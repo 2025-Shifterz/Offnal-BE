02e6b0c2a5eca7911f93ec9a4cec4587e5fb78fa
GOOHAESEUNG <amu6675@naver.com>
2025-06-27 15:03:50 +0900

    [FEAT][GOOHAESEUNG] 기존 로그인 성공 후 메시지 응답 필드 삭제 #8

--- src/main/java/com/offnal/shifterz/member/dto/AuthResponseDto.java
+++ src/main/java/com/offnal/shifterz/member/dto/AuthResponseDto.java
@@ -15,15 +15,12 @@ public class AuthResponseDto {
     @Schema(description = "신규 가입 여부")
     private boolean isNewMember;
 
-    @Schema(description = "응답 메시지")
-    private String message;
 
 
     public static AuthResponseDto from(Member member, boolean isNewMember) {
         return AuthResponseDto.builder()
                 .nickname(member.getKakaoNickname())
                 .isNewMember(isNewMember)
-                .message("로그인을 성공했습니다")
                 .build();
     }
 }
