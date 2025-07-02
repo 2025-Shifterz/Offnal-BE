4d06ef9ea43b6f7401884a11be5eecddf4438671
starday119 <jy8294@naver.com>
2025-07-02 22:31:20 +0900

    [FEAT/#14][jjaeroong]:캘린더 조회 Dto 설계

---
+++ src/main/java/com/offnal/shifterz/work/dto/WorkCalendarResponseDto.java
@@ -0,0 +1,12 @@
+package com.offnal.shifterz.work.dto;
+
+import com.offnal.shifterz.work.domain.WorkTimeType;
+
+import java.time.Month;
+
+public class WorkCalendarResponseDto {
+    private String day;
+    private String year;
+    private String month;
+    private WorkTimeType workTimeType;
+}
