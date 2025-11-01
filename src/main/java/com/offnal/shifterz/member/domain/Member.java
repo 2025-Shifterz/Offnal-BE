aa54361ad04c35d5457439b8fef29fe7fea48c73
yunnij <blrdyunnij@gmail.com>
2025-11-02 06:17:49 +0900

    [chore][yunnij] - member 엔티티에 UniqueConstraint 추가

--- src/main/java/com/offnal/shifterz/member/domain/Member.java
+++ src/main/java/com/offnal/shifterz/member/domain/Member.java
@@ -10,15 +10,27 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @Getter
+@Table(
+        name = "member",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uk_provider_provider_id",
+                        columnNames = {"provider", "provider_id"}
+                )
+        }
+)
 public class Member extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Provider provider;
 
+    @Column(name = "provider_id", nullable = false)
     private String providerId;
+
     private String email;
     private String memberName;
     private String phoneNumber;
