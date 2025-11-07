8432251f7d194a9e3e228332a2115e332ea353af
starday119 <jy8294@naver.com>
2025-11-07 22:19:21 +0900

    [Refactor[jjaeroong]: 사용하지 않는 메서드 삭제

--- src/main/java/com/offnal/shifterz/home/converter/WorkScheduleConverter.java
+++ src/main/java/com/offnal/shifterz/home/converter/WorkScheduleConverter.java
@@ -1,10 +1,7 @@
 package com.offnal.shifterz.home.converter;
 
-import com.offnal.shifterz.home.dto.WorkScheduleContext;
 import com.offnal.shifterz.home.dto.WorkScheduleResponseDto;
-import com.offnal.shifterz.work.domain.WorkTime;
 import com.offnal.shifterz.work.domain.WorkTimeType;
-import java.time.LocalDate;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,25 +13,4 @@ public class WorkScheduleConverter {
                 .build();
     }
 
-    public WorkScheduleResponseDto toDto(WorkScheduleContext context) {
-        return WorkScheduleResponseDto.builder()
-                .todayType(context.getTodayType())
-                .build();
-    }
-
-    public WorkScheduleContext toContext(
-            LocalDate date,
-            WorkTimeType yesterdayType,
-            WorkTimeType todayType,
-            WorkTimeType tomorrowType,
-            WorkTime workTime
-    ) {
-        return WorkScheduleContext.builder()
-                .date(date)
-                .yesterdayType(yesterdayType)
-                .todayType(todayType)
-                .tomorrowType(tomorrowType)
-                .workTime(workTime)
-                .build();
-    }
 }
