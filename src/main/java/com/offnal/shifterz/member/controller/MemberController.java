1784979d3336458f00b3b0c303a71239fec6481d
yunnij <blrdyunnij@gmail.com>
2025-11-15 15:22:48 +0900

    [fix][yunnij] - bean 의존성 수정

--- src/main/java/com/offnal/shifterz/member/controller/MemberController.java
+++ src/main/java/com/offnal/shifterz/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.offnal.shifterz.member.controller;
 
+import com.offnal.shifterz.global.common.AuthService;
 import com.offnal.shifterz.global.exception.ErrorApiResponses;
 import com.offnal.shifterz.global.response.SuccessApiResponses;
 import com.offnal.shifterz.global.response.SuccessCode;
@@ -7,6 +8,7 @@ import com.offnal.shifterz.global.response.SuccessResponse;
 import com.offnal.shifterz.global.util.S3Service;
 import com.offnal.shifterz.global.util.dto.PresignedUrlRequestDto;
 import com.offnal.shifterz.global.util.dto.PresignedUrlResponse;
+import com.offnal.shifterz.member.domain.Member;
 import com.offnal.shifterz.member.dto.MemberRequestDto;
 import com.offnal.shifterz.member.dto.MemberResponseDto;
 import com.offnal.shifterz.member.service.MemberService;
@@ -41,6 +43,12 @@ public class MemberController {
             @RequestBody PresignedUrlRequestDto request
     ) {
         PresignedUrlResponse response = s3Service.generateUploadPresignedUrl(request.getExtension());
+        Member member = AuthService.getCurrentMember();
+
+        if (member.getProfileImageKey() == null || member.getProfileImageKey().isEmpty()) {
+            memberService.updateProfileImage(response.getKey());
+        }
+
         memberService.updateProfileImage(response.getKey());
         return SuccessResponse.success(SuccessCode.PROFILE_UPLOAD_URL_CREATED, response);
     }
