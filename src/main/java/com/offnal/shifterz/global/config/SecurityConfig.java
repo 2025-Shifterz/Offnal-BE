7f65318f3436bf27093415215874e1957041b7e3
starday119 <jy8294@naver.com>
2026-01-31 00:05:39 +0900

    [OFFNAL-17/refactor][jjaeroong]: SwaggerConfig 및 SecurityConfig 하드코딩된 Uri 삭제 및 환경변수 적용

--- src/main/java/com/offnal/shifterz/global/config/SecurityConfig.java
+++ src/main/java/com/offnal/shifterz/global/config/SecurityConfig.java
@@ -1,10 +1,9 @@
 package com.offnal.shifterz.global.config;
 
-import com.offnal.shifterz.global.exception.CustomAuthenticationEntryPoint;
-import com.offnal.shifterz.global.util.RedisUtil;
-import com.offnal.shifterz.jwt.JwtAuthenticationFilter;
-import com.offnal.shifterz.jwt.JwtTokenProvider;
-import lombok.RequiredArgsConstructor;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,75 +15,85 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-import java.util.List;
+import com.offnal.shifterz.global.exception.CustomAuthenticationEntryPoint;
+import com.offnal.shifterz.global.util.RedisUtil;
+import com.offnal.shifterz.jwt.JwtAuthenticationFilter;
+import com.offnal.shifterz.jwt.JwtTokenProvider;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtTokenProvider jwtTokenProvider;
-    private final RedisUtil redisUtil;
-    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+	private final JwtTokenProvider jwtTokenProvider;
+	private final RedisUtil redisUtil;
+	private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+	@Value("${cors.allowed-origins}")
+	String allowedOrigins;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().and() //CORS 활성화 추가
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests.requestMatchers(
-                                        "/login/**",
-                                        "/login/page/**",
-                                        "/tokens/reissue",
-                                        "/callback/**",
-                                        "/swagger-ui.html",
-                                        "/swagger-ui/**",
-                                        "/v3/api-docs/**",
-                                        "/error",
-                                        "/actuator/health",
-                                        "/actuator/**").permitAll()
-                                .anyRequest().authenticated()
-                )
-                .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, redisUtil, customAuthenticationEntryPoint),
-                        UsernamePasswordAuthenticationFilter.class);
-        return http.build();
-    }
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.cors().and() //CORS 활성화 추가
+			.authorizeHttpRequests(authorizeRequests ->
+				authorizeRequests.requestMatchers(
+						"/login/**",
+						"/login/page/**",
+						"/tokens/reissue",
+						"/callback/**",
+						"/swagger-ui.html",
+						"/swagger-ui/**",
+						"/v3/api-docs/**",
+						"/error",
+						"/actuator/health",
+						"/actuator/**").permitAll()
+					.anyRequest().authenticated()
+			)
+			.csrf(AbstractHttpConfigurer::disable)
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, redisUtil, customAuthenticationEntryPoint),
+				UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+		/*
+		 * 1) Apple callback은 Origin:null 요청
+		 * 모든 Origin 허용 필요
+		 */
+		CorsConfiguration appleCors = new CorsConfiguration();
+		appleCors.setAllowedOriginPatterns(List.of("*")); // Origin:null 허용
+		appleCors.setAllowedMethods(List.of("GET", "POST", "OPTIONS"));
+		appleCors.setAllowedHeaders(List.of("*"));
+		appleCors.setAllowCredentials(true);
+
+		source.registerCorsConfiguration("/callback/**", appleCors);
 
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+		/*
+		 * 일반 API는 Origin 제한
+		 */
+		CorsConfiguration normalCors = new CorsConfiguration();
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		List<String> origins = Arrays.stream(allowedOrigins.split(","))
+			.map(String::trim)
+			.filter(s -> !s.isEmpty())
+			.toList();
 
-        /*
-         * 1) Apple callback은 Origin:null 요청
-         * 모든 Origin 허용 필요
-         */
-        CorsConfiguration appleCors = new CorsConfiguration();
-        appleCors.setAllowedOriginPatterns(List.of("*")); // Origin:null 허용
-        appleCors.setAllowedMethods(List.of("GET", "POST", "OPTIONS"));
-        appleCors.setAllowedHeaders(List.of("*"));
-        appleCors.setAllowCredentials(true);
+		normalCors.setAllowedOrigins(origins);
+		normalCors.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
+		normalCors.setAllowedHeaders(List.of("*"));
+		normalCors.setAllowCredentials(true);
 
-        source.registerCorsConfiguration("/callback/**", appleCors);
+		source.registerCorsConfiguration("/**", normalCors);
 
-
-        /*
-         * 일반 API는 Origin 제한
-         */
-        CorsConfiguration normalCors = new CorsConfiguration();
-        normalCors.setAllowedOrigins(List.of("https://api.offnal.site", "http://localhost:8081"));
-        normalCors.setAllowedMethods(List.of("GET", "POST", "PATCH", "DELETE", "OPTIONS"));
-        normalCors.setAllowedHeaders(List.of("*"));
-        normalCors.setAllowCredentials(true);
-
-        source.registerCorsConfiguration("/**", normalCors);
-
-        return source;
-    }
-
+		return source;
+	}
 
 }
