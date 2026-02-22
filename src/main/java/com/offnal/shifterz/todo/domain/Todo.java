70894b32874e48aaf2e945d2d65a8035c9214a26
starday119 <jy8294@naver.com>
2026-02-23 00:55:37 +0900

    [OFFNAL-32/feat][jjaeroong]: 기존 데이터 암호화 마이그레이션

--- src/main/java/com/offnal/shifterz/todo/domain/Todo.java
+++ src/main/java/com/offnal/shifterz/todo/domain/Todo.java
@@ -46,8 +46,13 @@ public class Todo extends BaseTimeEntity {
     // }
 
     public void update(TodoRequestDto.UpdateDto request, String encryptedContent) {
-        if (request.getContent() != null) this.content = encryptedContent; // ✅ 암호문 저장
+        if (request.getContent() != null) this.content = encryptedContent; // 암호문 저장
         if (request.getCompleted() != null) this.completed = request.getCompleted();
         if (request.getTargetDate() != null) this.targetDate = request.getTargetDate();
+    }
+
+    //마이그레이션 후 삭제
+    public void migrateContent(String encryptedContent) {
+        this.content = encryptedContent;
     }
 }
